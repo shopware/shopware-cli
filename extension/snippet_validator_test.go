f3d52d0c2aa3ed034c243700a548cd384f6a3bb2
Marcel Kräml <m.kraeml@shopware.com>
2025-10-09 15:54:25 +0200

    feat: support country-agnostic language files

--- extension/snippet_validator_test.go
+++ extension/snippet_validator_test.go
@@ -96,3 +96,21 @@ func TestSnippetValidateFindsInvalidJsonInGermanFile(t *testing.T) {
 	assert.Len(t, check.Results, 1)
 	assert.Contains(t, check.Results[0].Message, "contains invalid JSON")
 }
+
+func TestSnippetValidatePrioritizesCountryAgnosticEnglish(t *testing.T) {
+	tmpDir := t.TempDir()
+	check := &testCheck{}
+
+	_ = os.MkdirAll(path.Join(tmpDir, "Resources", "snippet"), os.ModePerm)
+	_ = os.WriteFile(path.Join(tmpDir, "Resources", "snippet", "storefront.en.json"), []byte(`{"a": "1"}`), os.ModePerm)
+	_ = os.WriteFile(path.Join(tmpDir, "Resources", "snippet", "storefront.en-GB.json"), []byte(`{"a": "1", "b": "2"}`), os.ModePerm)
+	_ = os.WriteFile(path.Join(tmpDir, "Resources", "snippet", "storefront.de-DE.json"), []byte(`{"a": "3"}`), os.ModePerm)
+
+	assert.NoError(t, validateStorefrontSnippetsByPath(tmpDir, tmpDir, check))
+	assert.Len(t, check.Results, 1)
+	assert.Contains(
+		t,
+		check.Results[0].Message,
+		"en-GB.json, missing key \"/b\" in this snippet file, but defined in the main language",
+	)
+}
