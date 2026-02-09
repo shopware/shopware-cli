9c9378ba9d36e05484de96f490fdd8e6ae2e3e24
Shyim <s.sayakci@gmail.com>
2026-02-09 22:24:08 +0100

    fix: remove hard-code 6.7.0.0 to trunk

--- internal/extension/setup.go
+++ internal/extension/setup.go
@@ -17,7 +17,7 @@ func setupShopwareInTemp(ctx context.Context, minVersion string) (string, error)
 
 	branch := "v" + strings.ToLower(minVersion)
 
-	if minVersion == DevVersionNumber || minVersion == "6.7.0.0" {
+	if minVersion == DevVersionNumber {
 		branch = "trunk"
 	}
 
