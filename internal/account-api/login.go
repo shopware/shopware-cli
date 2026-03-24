798b8a0dad11e7c5191d8c24dfb1eb6f0d40a964
Soner Sayakci <s.sayakci@gmail.com>
2026-03-24 07:52:29 +0100

    feat(account): add deprecation warning for username/password authentication
    Warn users that username/password authentication is deprecated and will be
    removed in August 2026, directing them to OAuth2 client credentials.

--- internal/account-api/login.go
+++ internal/account-api/login.go
@@ -38,6 +38,7 @@ func NewApi(ctx context.Context) (*Client, error) {
 	password := os.Getenv("SHOPWARE_CLI_ACCOUNT_PASSWORD")
 
 	if email != "" && password != "" {
+		logging.FromContext(ctx).Warnf("authentification with username/password is deprecated and will be removed August 2026. Please switch to OAuth2 client credentials, see https://developer.shopware.com/docs/products/cli/shopware-account-commands/authentication.html")
 		return loginWithCredentials(ctx, email, password)
 	}
 
