e5bb8348cd1a312e489855d9b0ecf622b88e7107
Soner <s.sayakci@gmail.com>
2026-07-13 20:42:18 +0200

    Fix bin/ci command invocation (#1191)
    * Fix bin/ci command invocation
    
    * Fix CI test import ordering

--- cmd/project/ci_test.go
+++ cmd/project/ci_test.go
@@ -9,7 +9,19 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/shopware/shopware-cli/internal/executor"
 )
+
+func TestBinCICommand(t *testing.T) {
+	cmdExecutor := executor.NewLocal("/project")
+
+	version := binCICommand(t.Context(), cmdExecutor, "--version")
+	assert.Equal(t, []string{"php", "bin/ci", "--version"}, version.Cmd.Args)
+
+	assetInstall := binCICommand(t.Context(), cmdExecutor, "asset:install")
+	assert.Equal(t, []string{"php", "bin/ci", "asset:install"}, assetInstall.Cmd.Args)
+}
 
 func TestGenerateProjectSBOM(t *testing.T) {
 	root := t.TempDir()
