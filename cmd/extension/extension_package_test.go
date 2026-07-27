4cfcc64e357d64bfbe9a290628ddcc79ec69129e
Soner <github@shyim.de>
2026-07-27 08:59:21 +0000

    feat(extension): rename `extension zip` to `extension package`
    Introduce `extension package` as the primary command name and keep
    `extension zip` as a deprecated alias for backward compatibility.
    
    Closes #1256

--- cmd/extension/extension_package_test.go
+++ cmd/extension/extension_package_test.go
@@ -9,20 +9,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestZipDoesNotDeleteUnrelatedZipsInWorkingDirectory(t *testing.T) {
+func TestPackageDoesNotDeleteUnrelatedZipsInWorkingDirectory(t *testing.T) {
 	extDir := t.TempDir()
 	require.NoError(t, os.WriteFile(filepath.Join(extDir, "composer.json"), []byte(`{
-		"name": "frosh/frosh-test",
-		"type": "shopware-platform-plugin",
-		"license": "MIT",
-		"version": "1.0.0",
-		"require": { "shopware/core": "~6.6.0" },
-		"autoload": { "psr-4": { "FroshTest\\": "src/" } },
-		"extra": {
-			"shopware-plugin-class": "FroshTest\\FroshTest",
-			"label": { "de-DE": "Test", "en-GB": "Test" }
-		}
-	}`), 0o644))
+			"name": "frosh/frosh-test",
+			"type": "shopware-platform-plugin",
+			"license": "MIT",
+			"version": "1.0.0",
+			"require": { "shopware/core": "~6.6.0" },
+			"autoload": { "psr-4": { "FroshTest\\": "src/" } },
+			"extra": {
+				"shopware-plugin-class": "FroshTest\\FroshTest",
+				"label": { "de-DE": "Test", "en-GB": "Test" }
+			}
+		}`), 0o644))
 	require.NoError(t, os.WriteFile(filepath.Join(extDir, ".shopware-extension.yml"), []byte(
 		"build:\n  zip:\n    composer:\n      enabled: false\n    assets:\n      enabled: false\n",
 	), 0o644))
@@ -39,15 +39,15 @@ func TestZipDoesNotDeleteUnrelatedZipsInWorkingDirectory(t *testing.T) {
 	t.Cleanup(func() { disableGit = false })
 
 	outputDir := filepath.Join(workDir, "dist")
-	extensionZipCmd.SetContext(t.Context())
-	require.NoError(t, extensionZipCmd.Flags().Set("output-directory", outputDir))
-	require.NoError(t, extensionZipCmd.Flags().Set("filename", "custom-name.zip"))
+	extensionPackageCmd.SetContext(t.Context())
+	require.NoError(t, extensionPackageCmd.Flags().Set("output-directory", outputDir))
+	require.NoError(t, extensionPackageCmd.Flags().Set("filename", "custom-name.zip"))
 	t.Cleanup(func() {
-		_ = extensionZipCmd.Flags().Set("output-directory", "")
-		_ = extensionZipCmd.Flags().Set("filename", "")
+		_ = extensionPackageCmd.Flags().Set("output-directory", "")
+		_ = extensionPackageCmd.Flags().Set("filename", "")
 	})
 
-	require.NoError(t, extensionZipCmd.RunE(extensionZipCmd, []string{extDir}))
+	require.NoError(t, extensionPackageCmd.RunE(extensionPackageCmd, []string{extDir}))
 
 	assert.FileExists(t, decoyBackup)
 	assert.FileExists(t, decoyRelease)
