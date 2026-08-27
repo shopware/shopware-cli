622b2dbeb7688ee6192c446934e924741b3e2629
Anne Hintzpeter <a.hintzpeter@shopware.com>
2026-08-27 14:06:56 +0200

    fix: adjust to updated reqs

--- cmd/extension/extension_create.go
+++ cmd/extension/extension_create.go
@@ -21,17 +21,6 @@ func newCreateCmd() *cobra.Command {
 	// Define flags for the command
 	fs := cmd.Flags()
 	fs.StringVar(&opts.Name, "name", "", "Name of the extension")
-	fs.StringVarP(&opts.Namespace, "namespace", "s", "", "Namespace of the extension")
-	fs.BoolVarP(&opts.AllExamples, "all-examples", "a", false, "Include all examples")
-	fs.BoolVar(&opts.ConsoleCommand, "console-command", false, "Include a console command")
-	fs.BoolVar(&opts.ScheduledTask, "scheduled-task", false, "Include a scheduled task")
-	fs.BoolVar(&opts.EventSubscriber, "event-subscriber", false, "Include an event subscriber")
-	fs.BoolVar(&opts.Controller, "controller", false, "Include a controller")
-	fs.BoolVar(&opts.Route, "route", false, "Include a route")
-	fs.StringVar(&opts.Entities, "entities", "", "Include entities")
-	fs.BoolVar(&opts.JavascriptPlugin, "javascript-plugin", false, "Include a JavaScript plugin")
-	fs.BoolVar(&opts.AdminModule, "admin-module", false, "Include an admin module")
-	fs.BoolVar(&opts.CustomFieldset, "custom-fieldset", false, "Include a custom fieldset")
 
 	return cmd
 }
